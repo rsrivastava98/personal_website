e175fa9a0635922c5ab2f8b29b60d75003d4c51f
Rudra Srivastava <rudrasrivastava@Rudras-MacBook-Pro.local>
2020-05-17 03:22:13 +0530

    added viz plus fixes

--- src/main.js
+++ src/main.js
@@ -555,7 +555,7 @@ export default function main(d3, football, locations){
     // Add your JavaScript code here
 const MAX_WIDTH = Math.max(1080, window.innerWidth);
 const MAX_HEIGHT = 720;
-const margin = {top: 40, right: 100, bottom: 40, left: 175};
+const margin = {top: 40, right: 100, bottom: 40, left: 100};
 
 // Assumes the same graph width, height dimensions as the example dashboard. Feel free to change these if you'd like
 let graph_1_width = (MAX_WIDTH / 2) - 10, graph_1_height = 250;
